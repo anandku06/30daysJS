16567f7f7bd25612670f4246d2f0cecea010f961
Anand Kumar <161126335+anandku06@users.noreply.github.com>
2024-07-14 14:58:44 +0530

    Done with Day 1 tasks.

---
+++ Day1.js
@@ -0,0 +1,38 @@
+//Task 1 : Declare a variable using 'var', assign it a number, and log the value to the console.
+var num = 10;
+console.log(num);
+
+//Task 2 : Declare a variable using 'let', assign it a string, and log the value to the console.
+let str = "Hello";
+console.log(str);
+
+//Task 3 : Declare a variable using 'const', assign it a boolean, and log the value to the console.
+const bool = true;
+console.log(bool);
+
+//Task 4 : Create variable of different data types (number, string, boolean, object, array) and log each variable's type using typeof operator.
+var num1 = 10;
+console.log(typeof num1);
+
+var str1 = "Hello";
+console.log(typeof str1);
+
+var bool1 = true;
+console.log(typeof bool1);
+
+var obj = { name: "John", age: 25 };
+console.log(typeof obj);
+
+var arr = [1, 2, 3, 4, 5];
+console.log(typeof arr);
+
+//Task 5 : Declare a variable using 'let', assign it an intial value, reaasign a new value, and log both values to the console.
+let x = 5;
+console.log(x);
+x = 10;
+console.log(x);
+
+//Task 6 : Try reassigning a value to a variable declared using 'const' and observe the error.
+const y = 20;
+y = 25;
+console.log(y);
