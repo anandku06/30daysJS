33f714a544227fa3babbfcf313655fcbf087e67c
Commiter <161126335+anandku06@users.noreply.github.com>
2024-11-14 23:55:11 +0530

    Revised Datatypes in JS

---
+++ 02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict" // treat all JS code as newer version of JS
+
+// alert("hello!!") // gives error in terminal; only accesible in browser's console
+
+console.log(3 +
+    3
+); // not a syntax error but also not a good practise as well; decreases code readability
+
+let name = "Anand" // it's a string datatype; enclosed in double quotes
+
+let age = 98 // number datatype;  >= 2 raised to the power 53 
+
+let isEligible = true // boolean datatype; true or false / 1 or 0
+
+// bigInt datatype; used to handle big numbers larger than number datatype range
+
+// null datatype and a standalone value; empty value or can't be fetched
+// undefined : value not assigned yet
+// symbol : defines uniqueness of a component(React ref)
+// object
+
+console.log(typeof "Lauyda"); // outputs string : tells the datatype of the given data
+
+console.log(typeof null); // give object 
+console.log(typeof undefined); // give undefined
