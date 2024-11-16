3502da720aac656169c878b2220c6ae2c2a85ac3
Commiter <161126335+anandku06@users.noreply.github.com>
2024-11-16 23:30:49 +0530

    Learned Operations in JS

--- 03_conversionOperations.js
+++ 03_conversionOperations.js
@@ -34,4 +34,37 @@ let stringNumber = String(someNumber)
 console.log(typeof(stringNumber)); // outputs string : converts number to string
 
 
- // ================== Operations ============================
+//  ================== Operations ============================
+
+let value = 3
+let negValue = -value
+console.log(negValue); // console logs the negative of the value
+
+console.log(2 + 2);
+console.log(2 - 2);
+console.log(2 * 2);
+console.log(2 ** 2); // exponential operator (here 2 raised to the power 2)
+console.log(2 / 2);
+console.log(2 % 2); // floor division : returns the remainder of the division
+
+let str1 = "Hitesh"
+let str2 = " Coder"
+let str3 = str1 + str2
+console.log(str3); // logs the additon of two string ; also called concatenation of strings
+
+str3 = "1" + 2
+console.log(str3); // logs 12 : here 2 is changed to string and concatenation also happens
+console.log(typeof str3); // outputs string : as the conversion is started from left to right so if string is encountered so after it all the things are converted to string
+
+str3 = 1 + 2 + "3"
+console.log(str3); // outputs 33 : number are added as usual but then string is encountered so concatenation occurs and final type is of string
+console.log(typeof str3); // string
+
+console.log(+true); // 1 : boolean conversion
+// console.log(true+); // not allowed throws error
+
+console.log(+"") // 0 
+
+/* Post Increment / Decrement : increase or decrease after usage
+   Pre Increment / Decrement : increase or decrease before usage
+*/ 
