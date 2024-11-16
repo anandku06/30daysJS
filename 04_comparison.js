7e1a9440341053efe122c495f7b70595c3927198
Commiter <161126335+anandku06@users.noreply.github.com>
2024-11-16 23:39:07 +0530

    Learned Comparison Operators in JS

---
+++ 04_comparison.js
@@ -0,0 +1,11 @@
+ /* Comparison Operators : < , > , <= , >= , == , !=
+ returns true or false 
+ try to compare two same datatypes for expected results */
+//  console.log(2 > 1); // true
+//  console.log(2 < 1); // true
+//  console.log(2 >= 1); // true
+//  console.log(2 >= 1); // true
+//  console.log(2 == 1); // true
+//  console.log(2 != 1); // true
+  
+console.log("2" > 1); // converts string "2" to number and then compared so here logs true
