0c12bc208e99249c29d2b368eb27a1a5b3272897
Commiter <161126335+anandku06@users.noreply.github.com>
2024-11-17 00:04:23 +0530

    More in datatypes

--- 02_datatypes.js
+++ 02_datatypes.js
@@ -1,3 +1,10 @@
+/* On the basis of the allocation of the datatypes in memory; there are two types of datatypes :
+> Primitive : 7 types (String, Number, Boolean, null, undefined, symbol, BigInt) ; Call by Value method used
+
+> Reference (Non-Primitive) : Array, Objects, Functions ; Call by Reference method
+*/
+
+
 "use strict" // treat all JS code as newer version of JS
 
 // alert("hello!!") // gives error in terminal; only accesible in browser's console
@@ -9,6 +16,7 @@ console.log(3 +
 let name = "Anand" // it's a string datatype; enclosed in double quotes
 
 let age = 98 // number datatype;  >= 2 raised to the power 53 
+let interestRate = 9.8 // number datatype : float numbers also considered under Number
 
 let isEligible = true // boolean datatype; true or false / 1 or 0
 
@@ -23,3 +31,8 @@ console.log(typeof "Lauyda"); // outputs string : tells the datatype of the give
 
 console.log(typeof null); // give object 
 console.log(typeof undefined); // give undefined
+
+
+const id = Symbol("123") // returns symbol
+const anotherid = Symbol("123") // returns symbol
+console.log(id === anotherid); // false
