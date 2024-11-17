3cd0c7edb72a6c2989d4bfb60f74f03499888114
Commiter <161126335+anandku06@users.noreply.github.com>
2024-11-18 00:21:04 +0530

    Done with Basics of Number Datatype

---
+++ 01_basics/07_numsAndMaths.js
@@ -0,0 +1,22 @@
+let score = 100
+console.log(score); // JS implicitly defines this data as Number
+
+
+let newScore = new Number(130) // Here we explicitly defined this as a Number datatype using 'new' keyword ; made an instance of Number class
+console.log(newScore); // outputs it as a object [Number : 130]
+// having some methods which can be seen in browser's console
+
+console.log(newScore.toString()); // a method used to convert the number to string
+console.log(typeof newScore.toString()); // outputs string bcz converts to string from number
+
+console.log(newScore.toFixed(2)); // a method used to set the number of precision digits after decimal; here digits will be 2 after decimal
+
+let othenNum = 1223.8966
+console.log(othenNum.toPrecision(2)); // takes 3 digit to precise 
+// Returns a string containing a number represented either in exponential(if specified digit is smaller than the total digit) or fixed-point notation with a specified number of digits.
+// basically rounds off the number according to the given digit
+
+let price = 10000000
+console.log(price.toLocaleString());// default is Indian here ; 1,00,00,000
+// a method used to convert the number to string but in local currency word i.e. in price
+
