fe243eed813e62c04dfdaa8df07f76db52d326c5
Commiter <161126335+anandku06@users.noreply.github.com>
2024-11-18 00:35:26 +0530

    Some methods of Math Object

--- 01_basics/07_numsAndMaths.js
+++ 01_basics/07_numsAndMaths.js
@@ -23,3 +23,17 @@ console.log(price.toLocaleString());// default is Indian here ; 1,00,00,000
 // max size of number can be seen using methods MAX_SAFE_INTEGER or MIN_SAFE_INTEGER
 
 // ========================== MATHS =============================
+
+/* Math is an object in JS which has many methods and functions defined to it */
+
+console.log(Math.abs(5)); // returns the absolute value of the specified number i.e. converts negative number to positive
+
+console.log(Math.round(8.1)); // The value to be rounded to the nearest integer. Returns a supplied numeric expression rounded to the nearest integer.
+
+console.log(Math.ceil(8.3));
+console.log(Math.floor(8.3));
+// ceil rounds up with greater than the specified integer
+// floor just does vice-versa
+
+console.log(Math.min(6,7,89,43,56)); // returns the minimum element from the specified array 
+console.log(Math.max(6,7,89,43,56)); // returns the maximum element from the specified array
