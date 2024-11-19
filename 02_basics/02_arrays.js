de44c160c8f8881938ca8a57eb58bbd5f2829a76
Commiter <161126335+anandku06@users.noreply.github.com>
2024-11-20 01:30:48 +0530

    Updating the file

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -1,16 +1,16 @@
 const marvelHeros = ["Thor", "SpiderMan", "IronMan"]
 const DCHeros = ["SuperMan", "Flash", "BatMan"]
 
-// marvelHeros.push(DCHeros) // inserted DCHeros in marvelHeros
-// console.log(marvelHeros); // output is array into an array DCHeros in marvelHeros; arrays are not merged but got inserted
+marvelHeros.push(DCHeros) // inserted DCHeros in marvelHeros
+console.log(marvelHeros); // output is array into an array DCHeros in marvelHeros; arrays are not merged but got inserted
 
-// console.log(marvelHeros[3][1]); // to access element which is in an array in another array we use this type of indexing
+console.log(marvelHeros[3][1]); // to access element which is in an array in another array we use this type of indexing
 
-// const newHeros = marvelHeros.concat(DCHeros) // concatenates two arrays and returns a new array
-// console.log(newHeros);
+const newHeros = marvelHeros.concat(DCHeros) // concatenates two arrays and returns a new array
+console.log(newHeros);
 
-const newHeros = [...marvelHeros, ...DCHeros] // '...' is called spread operator; used to include elements of an array in a new array
-// console.log(newHeros);
+const newHero = [...marvelHeros, ...DCHeros] // '...' is called spread operator; used to include elements of an array in a new array
+console.log(newHero);
 
 const anotherArray = [1, 2, 3,[4, 5, 6], 7,[6, 7,[4, 5]]]
 const anoArray = anotherArray.flat(Infinity) // Returns a new array with all sub-array elements concatenated into it recursively up to the specified depth. Infinity value specifies the depth on its own. 
