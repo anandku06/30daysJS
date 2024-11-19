ff7761f215c80b8e1a5d91dd701406eda5bdaa57
Commiter <161126335+anandku06@users.noreply.github.com>
2024-11-19 16:17:19 +0530

    Some more methods in Array

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -19,4 +19,25 @@ myArr.pop() // Removes the last element from an array and returns it. If the arr
 myArr.unshift(8) // insert the element at the start of the array
 myArr.shift() // Removes the first element from an array and returns it. If the array is empty, undefined is returned and the array is not modified.
 
+console.log(myArr.includes(8)) // Determines whether an array includes a certain element, returning true or false as appropriate.
+console.log(myArr.indexOf(2)) // Returns the index of the first occurrence of a value in an array, or -1 if it is not present.
+
+const newArr = myArr.join() // Adds all the elements of an array into a string, separated by the specified separator string. default is comma seperator
+
+console.log(newArr);
+console.log(typeof(newArr));
 console.log(myArr);
+
+const my1 = myArr.slice(1, 3) // Returns a copy of a section of an array. For both start and end(exclusive), a negative index can be used to indicate an offset from the end of the array
+
+console.log(my1);
+console.log('A', myArr);
+
+const my2 = myArr.splice(1, 3) // Removes elements from an array and, if necessary, inserts new elements in their place, returning the deleted elements.
+
+console.log(my2);
+console.log('B', myArr);
+
+/* Main difference in slice and splice are -
+In slice the end index is excluded and orginal array is not affected
+But in splice, the end index is included and the range is also deleted from the original array */
