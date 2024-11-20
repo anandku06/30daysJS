0a8a28e068ecf8da1c1712db8344a6cb4f9f8f17
Commiter <161126335+anandku06@users.noreply.github.com>
2024-11-21 04:30:37 +0530

    Destructuring of Objects in JS

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -58,3 +58,30 @@ console.log(Object.keys(tinderUser)); // returns the keys of the specified objec
 console.log(Object.values(tinderUser)); // returns the values of the specified object in array form so iterable
 console.log(Object.entries(tinderUser)); // Returns an array of key/values of the enumerable own properties of an object
 console.log(tinderUser.hasOwnProperty('isLoggedIn')); // Determines whether an object has a property with the specified name.
+
+// Destructring of Objects in JS : a syntactical sugar for better code readability
+const course = 
+{
+    courseName : "JS in Hindi",
+    price : "999/-",
+    courseInstructor : {
+        male : "Hitesh",
+        female : "HogiKoi"
+    }
+}
+
+const {courseName, courseInstructor, price } = course 
+// here the keys of the object course are extracted and assigned to variables with the same names
+const {courseInstructor : teacher} = course
+//here if the keys name can also be changed using (objName : newName) while extracting
+const {platform = "Youtube"} = course
+// default params can also be given when a key is undefined or missing like platform : "YouTube"
+const {teacher : {male, female}} = course
+// destructure nested objects by specifying the path to the properties
+const {email , ...otherDetails} = regularUser // rest operator collects the remaining properties if number of properties is more
+
+/*
+Rest Operator : used to collect the remaining elements or properties during destructuring in arrays or objects into a single variable ; gathers elements or properties that are not assigned yet to variables
+
+Spread Operator : used to spread out or expand the elements of an array or properties of an object ; expands the elements or properties so that they can be passed or assigned individually
+*/
