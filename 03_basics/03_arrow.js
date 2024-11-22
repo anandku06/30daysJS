99f24f821301f226125e2718df7481623e6c5a6d
Commiter <161126335+anandku06@users.noreply.github.com>
2024-11-23 03:05:59 +0530

    Some more concepts in Arrow functions and formatting

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -3,35 +3,35 @@ this keyword in JS is used to refer the current scope context in which it is use
  */
 
 const user = {
-    username : "Hitesh",
-    price : 99,
+  username: "Hitesh",
+  price: 99,
 
-    welcomeMessage : function(){
-        console.log(`${this.username}, Welcome to website!!`) // here also this refers to the user's object reference
-        console.log(this); // logs the object it signifies to
-    }
-}
+  welcomeMessage: function () {
+    console.log(`${this.username}, Welcome to website!!`); // here also this refers to the user's object reference
+    console.log(this); // logs the object it signifies to
+  },
+};
 
-user.welcomeMessage()
-user.username = "Sam" // changed the value of username in the user object
-user.welcomeMessage() // welcome mssg also changed bcz this signifies the values in the current scope it is being used so if the username is changed it'll take the updated value rather than the old value
+user.welcomeMessage();
+user.username = "Sam"; // changed the value of username in the user object
+user.welcomeMessage(); // welcome mssg also changed bcz this signifies the values in the current scope it is being used so if the username is changed it'll take the updated value rather than the old value
 console.log(this); // logs an empty object bcz here this refers to the reference of the current scope and here not any scope is defined in node environment
 // but this changes in browser's environment : this refers to the window object in browser's environment
 
-function chai(){
-    let username = "Hitesh"
-    console.log(this);  // outputs a number of listing that this function holds
+function chai() {
+  let username = "Hitesh";
+  console.log(this); // outputs a number of listing that this function holds
 
-    console.log(this.username); // undefined ; here this is not considering the context inside function, so it only works with objects
+  console.log(this.username); // undefined ; here this is not considering the context inside function, so it only works with objects
 }
 
 // same for functions declared using variables
-const chai = function(){
-    let username = "Hitesh"
-    console.log(this);  // outputs a number of listing that this function holds
+const chai = function () {
+  let username = "Hitesh";
+  console.log(this); // outputs a number of listing that this function holds
 
-    console.log(this.username); // undefined ; here this is not considering the context inside function, so it only works with objects
-}
+  console.log(this.username); // undefined ; here this is not considering the context inside function, so it only works with objects
+};
 
 /* Arrow Functions : shorthand for declaring functions in JS ; ES6 features ; 
 
@@ -41,9 +41,20 @@ funcName() this is calling of that function
 */
 
 const chai = () => {
-    const username = "hitesh"
-    console.log(this.username); // here also undefined
-    console.log(this); // logs empty object   
-}
+  const username = "hitesh";
+  console.log(this.username); // here also undefined
+  console.log(this); // logs empty object
+};
 
-chai()
+chai();
+
+const addTwo = (num1, num2) => {
+  return num1 + num2;
+}; // basic arrow function of adding two numbers
+
+const addTwo = (num1, num2) => num1 + num2; // shorthand of arrow function when function body is of a single statement : Implicit return
+// if used parenthesis then no return else return in arrow functions
+
+const addTwo = (num1, num2) => ({ num1: num2 }); // here parenthesis involved bcz useful for returning an object if necessary
+
+console.log(addTwo(5, 6));
