1df21d211cf3cf5d209208c6e9987ab2a303b2ab
Commiter <161126335+anandku06@users.noreply.github.com>
2024-11-24 14:35:22 +0530

    Add while loop example to two.js file

---
+++ 05_iterations/two.js
@@ -0,0 +1,16 @@
+/* While Loop : another type of loop used in JS ; checks a
+    condition according to that program executes in a loop ;
+    if true then loop continues else exits the loop ; but unlike
+    for loop it doesn't have any initialisation and updation so 
+    this to be given by the programmer.
+    
+Syntax:
+while( condition ){ body }
+*/
+
+let index = 0
+while(index <= 10){
+    console.log(`Value of index is ${index}`)
+    index += 2
+} /* example of a basic while loop ; initialisation and updation 
+    not done in the while loop like for loop */
