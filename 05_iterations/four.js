e963e03031af2798caf695eee74f9d51d26385aa
Commiter <161126335+anandku06@users.noreply.github.com>
2024-11-25 03:00:27 +0530

    Add for...in loop example for array iteration in four.js and clarify limitations with Maps

--- 05_iterations/four.js
+++ 05_iterations/four.js
@@ -18,3 +18,11 @@ for (const key in myObj) {
     console.log(`Languages are ${key} -> ${myObj[key]}`)
 } // to access the object's value, use key as index of the object like array indices
 
+const myArr = ['js', 'rb', 'python', 'java', 'cpp']
+
+for (const key in myArr) {
+    console.log(key) // outputs the indices
+    console.log(myArr[key]) // to access values : use it as accessing elements from an array
+} // in case of arrays : keys are the indices and values are the values on that indices
+
+// Maps are not iterable through for...in loop ; doesn't throw error but also doesn't give any output as well
