f80e51bd4c79a81a81e6e981c908d55c1cf318fe
Commiter <161126335+anandku06@users.noreply.github.com>
2024-11-25 03:44:16 +0530

    Refactor filter and forEach examples in six.js; add book objects array

--- 05_iterations/six.js
+++ 05_iterations/six.js
@@ -11,18 +11,28 @@ console.log(values); // undefined
 
 /* filter method : function that accepts up to three arguments. The filter method calls the predicate function one time for each element in the array. Returns the elements of an array that meet the condition specified in a callback function. */
 
-
 const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-const newNums = myNums.filter( (nums) => nums > 4) // filters the elements that are less than 4 and returns the elements greater than 4
+const newNums = myNums.filter((nums) => nums > 4); // filters the elements that are less than 4 and returns the elements greater than 4
 
-console.log(newNums)
+console.log(newNums);
 
 // alternative without using filter method
-const oldNums = []
+const oldNums = [];
 
 myNums.forEach((num) => {
-    if(num > 4)
-        oldNums.push(num);
-})
-console.log(oldNums)
+  if (num > 4) oldNums.push(num);
+});
+console.log(oldNums);
+
+const books = [
+  { title: "Book One", genre: "Fiction", publish: 1981, edition: 2004 },
+  { title: "Book Two", genre: "Non-Fiction", publish: 1992, edition: 2008 },
+  { title: "Book Three", genre: "History", publish: 1999, edition: 2007 },
+  { title: "Book Four", genre: "Non-Fiction", publish: 1989, edition: 2010 },
+  { title: "Book Five", genre: "Science", publish: 2009, edition: 2014 },
+  { title: "Book Six", genre: "Fiction", publish: 1987, edition: 2010 },
+  { title: "Book Seven", genre: "History", publish: 1986, edition: 1996 },
+  { title: "Book Eight", genre: "Science", publish: 2011, edition: 2016 },
+  { title: "Book Nine", genre: "Non-Fiction", publish: 1981, edition: 1989 },
+];
