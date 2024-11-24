c268d7af50db5365dc604d2e648d3104aa333867
Commiter <161126335+anandku06@users.noreply.github.com>
2024-11-25 03:18:27 +0530

    Add forEach method examples in five.js for array iteration

---
+++ 05_iterations/five.js
@@ -0,0 +1,29 @@
+/* forEach method : it's a method of Array object in JS ; makes
+    iteration on arrays very much efficent and easier ; targets 
+    each element in the array and calls the callback function on
+    each element ; has the access of the values, indices and the
+    whole array as well
+    
+    Syntax:
+    
+    myArray.forEach( callbackFunction ) // callbackFunction : can be anything, always an anonymous function
+*/
+
+const coding = ["js", "cpp", "python", "java", "ruby"]
+
+coding.forEach(function (value) {
+    console.log(value)
+}) // prints each element
+
+coding.forEach((item) => console.log(item)) // arrow functions can also be used here
+
+function printMe(item){
+    console.log(`Prints ${item}`)
+}
+
+coding.forEach(printMe) // external function can also be given here
+// only reference of the function is given(not included parenthesis)
+
+coding.forEach((item, index, arr) => 
+    console.log(item, index, arr)
+) // has the access of the whole array, indices and values
