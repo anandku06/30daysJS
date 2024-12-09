058eeb0e905e229d8b3b38e8ddf9f04fabdd1719
Commiter <161126335+anandku06@users.noreply.github.com>
2024-12-09 16:28:52 +0530

    Add documentation for promises in JavaScript with state explanations

---
+++ 09_advance_one/promises.js
@@ -0,0 +1,9 @@
+/* Promises in JavaScript -> an object represents the eventual
+    completion (or failure) of an asynchronous operation and its resulting value.
+
+    Having three states:
+    - pending -> initial state, neither fulfilled nor rejected
+    - fulfilled -> meaning that the operation was completed 
+        successfully
+    - rejected -> meaning that the operation failed
+*/
