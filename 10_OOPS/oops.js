96c1e41124a0fad2339ababab2fc999fa572c9e0
Commiter <161126335+anandku06@users.noreply.github.com>
2024-12-10 23:02:20 +0530

    Add constructor function example to illustrate 'this' context and object instantiation

--- 10_OOPS/oops.js
+++ 10_OOPS/oops.js
@@ -15,3 +15,17 @@ console.log(user.userName)
 console.log(this) // in node environment, this keyword refers to a empty object as there isn't a thing to refer globally
 // but in browser's console, this keyword returns a window object
 user.getUserDetails()
+
+// Constructor function
+/* new keyword is a constructor function here, used to create multiple instances of a class/object */
+const promiseOne = new Promise()
+const date = new Date()
+
+function User(userName, isLoggedIn, loginCount){
+    // userName = userName // this creates a ambiguity problem
+    this.userName = userName // 'this' keyword solves the problem by setting the context of the variable 'userName'
+    this.loginCount = loginCount
+    this.isLoggedIn = isLoggedIn
+
+    return this // returns the current context of the object
+}
