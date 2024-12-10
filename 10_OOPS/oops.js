b4e1167d974fd5783993d8379b2a010f6b8f238c
Commiter <161126335+anandku06@users.noreply.github.com>
2024-12-10 22:31:36 +0530

    Add object literal example for user data representation

---
+++ 10_OOPS/oops.js
@@ -0,0 +1,12 @@
+// Object Literal
+
+const user = {
+    userName : 'Hitesh',
+    loginCount : 8,
+    signedIn : true,
+
+    getUserDetails : function (){
+        console.log("Got user details from database")
+    }
+}
+
