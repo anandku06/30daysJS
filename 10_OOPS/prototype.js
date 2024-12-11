10fcba1f30713d935c44a6be65d0e5546b3c46f2
Commiter <161126335+anandku06@users.noreply.github.com>
2024-12-12 01:09:36 +0530

    Enhance prototype.js with examples of JavaScript inheritance and prototype chaining

--- 10_OOPS/prototype.js
+++ 10_OOPS/prototype.js
@@ -23,3 +23,26 @@ Object.prototype.trueLength = function(){
 // } // this can also be done here but it'll give this property to the String only, here the prototypical inheritance travels from Object to String, not String to Object
 
 console.log(myName.trueLength())
+
+/* Inheritance is done in JavaScript using __proto__ property which sets the proerty or data of one object to another object */
+
+const User = {
+    name : 'chai',
+    email : 'chai@google.com'
+}
+
+const teacher = {
+    makeVideo : true
+}
+
+const teachingSupport = {
+    isAvailable : false
+}
+
+const TASupport = {
+    makeAssignments: 'JS assignment',
+    fullTime : true,
+    __proto__ : teachingSupport
+}
+
+teacher.__proto__ = User // copies the properties of the User object to teacher object
