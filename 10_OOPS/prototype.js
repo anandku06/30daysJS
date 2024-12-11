6db7729d846c94f9a0053286ba661d07ada9a75a
Commiter <161126335+anandku06@users.noreply.github.com>
2024-12-12 00:43:07 +0530

    Add initial string length logging and comment for true length function in prototype.js

---
+++ 10_OOPS/prototype.js
@@ -0,0 +1,4 @@
+let myName = 'Anand'
+
+console.log(myName.length)
+// console.log(myName.trueLength) // aim is to make a function for every string to ensure the true length of it without considering any whitespaces
