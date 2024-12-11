47e1ec90080445ae0afc758c7323e09d7ad5b3e7
Commiter <161126335+anandku06@users.noreply.github.com>
2024-12-11 19:51:36 +0530

    Enhance comments in objects.js to clarify prototypal inheritance and prototype property usage; add createUser constructor function

--- 10_OOPS/objects.js
+++ 10_OOPS/objects.js
@@ -1,4 +1,5 @@
-/* Everything in JavaScript is an object, its default behaviour is prototypal, performs prototypal inheritance */
+/* Everything in JavaScript is an object, its default behaviour is prototypal, performs prototypal inheritance 
+*/
 
 function multiplyBy5(num){
     return num * 5
@@ -9,3 +10,16 @@ multiplyBy5.power = 2
 console.log(multiplyBy5(3))
 console.log(multiplyBy5.power)
 console.log(multiplyBy5.prototype)
+/* The prototype property in JavaScript is a fundamental concept
+    primarily associated with functions and objects.
+    It is a mechanism that allows JavaScript objects to inherit properties and methods from one another.
+
+    - Available on functions : every function has this property by default;
+        This property is an object that can be used to define properties and methods that will be inherited by all instances created by the function when used as a constructor.
+*/
+
+function createUser(userName, score){
+    // userName = userName // creates an ambiguity problem here
+    this.userName = userName // using 'this' keyword to fixing the context of the userName variable
+    this.score = score
+}
