a489c015d8a5f378fcfb34b4514315cc2c78632e
Commiter <161126335+anandku06@users.noreply.github.com>
2024-12-11 14:41:46 +0530

    Add multiplyBy5 function with static property and prototype logging

---
+++ 10_OOPS/objects.js
@@ -0,0 +1,11 @@
+/* Everything in JavaScript is an object */
+
+function multiplyBy5(num){
+    return num * 5
+}
+
+multiplyBy5.power = 2
+
+console.log(multiplyBy5(3))
+console.log(multiplyBy5.power)
+console.log(multiplyBy5.prototype)
