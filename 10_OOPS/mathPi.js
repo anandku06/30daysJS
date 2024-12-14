e3f3590672644af956d833c3f925930fa93158c4
Commiter <161126335+anandku06@users.noreply.github.com>
2024-12-15 01:08:28 +0530

    Update mathPi.js with comments on Object.getOwnPropertyDescriptor and log Math.PI value

--- 10_OOPS/mathPi.js
+++ 10_OOPS/mathPi.js
@@ -1,2 +1,5 @@
-Object.getOwnPropertyDescriptor() // Gets the own property descriptor of the specified object. 
-// An own property descriptor is one that is defined directly on the object and is not inherited from the object's prototype.
+/* Object.getOwnPropertyDescriptor() // Gets the own property descriptor of the specified object. 
+// An own property descriptor is one that is defined directly on the object and is not inherited from the object's prototype. */
+
+console.log(Math.PI)
+// Math.PI = 5  // value of Math.PI can't be overwritten
