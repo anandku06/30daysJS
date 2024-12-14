cbaaebdd9e5ca238027650c73971cf3e016617c9
Commiter <161126335+anandku06@users.noreply.github.com>
2024-12-14 18:07:43 +0530

    Add static methods example in User class to demonstrate static properties in OOP

---
+++ 10_OOPS/staticProp.js
@@ -0,0 +1,20 @@
+/* static keyword in JavaScript: used to define properties or methods that belong to a class itself, rather than to the instances of the class. 
+    Static methods and properties are shared across all instances of the class and are called on the class directly, not on individual objects
+*/
+
+class User{
+    constructor(userName){
+        this.userName = userName
+    }
+
+    logMe(){
+        console.log(`Username: ${this.userName}`)
+    }
+
+    static createID(){
+        return '123'
+    }
+}
+
+const user1 = new User('Rand')
+// console.log(user1.createID()) // trying access to static method then it throws an error
