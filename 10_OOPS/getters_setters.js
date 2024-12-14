93313e7c0755351da56073dac74a5b21c5c2e58b
Commiter <161126335+anandku06@users.noreply.github.com>
2024-12-15 03:54:28 +0530

    Add getter and setter for password in User class with validation logic

--- 10_OOPS/getters_setters.js
+++ 10_OOPS/getters_setters.js
@@ -1,9 +1,27 @@
+/* getter and setter in JS: methods that allow you to control the access to an object's properties.
+    They provide a way to encapsulate the logic for reading and writing property values, often adding validation, computation or other logic.
+    define getters and setters using the 'get' and 'set' keywords
+
+    getter and setter should be defined together, if defined only one then throws an error
+*/
+
 class User{
     constructor(email, password){
         this.email = email
         this.password = password
     }
+
+    get password(){
+        return `${this._password}_abcsdf12`
+    }
+
+    set password(value){
+        this._password = value.toUpperCase()
+    }
 }
 
-const userOne = new User('anand@chatgpt.com', 1234)
+// use '_propertyName' for the underlying value of a property when using getters and setters
+// if not then a error is thrown i.e. Maximum callStack exceeded
+const userOne = new User('anand@chatgpt.com', 'agstdf')
+userOne.password = 'asdgauydgyuad'
 console.log(userOne.password)
