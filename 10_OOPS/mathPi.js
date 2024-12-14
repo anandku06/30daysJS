70d71c962001fe7b0af1d5e4d4487f145f8bfaf1
Commiter <161126335+anandku06@users.noreply.github.com>
2024-12-15 01:04:31 +0530

    Add description for Object.getOwnPropertyDescriptor in mathPi.js

---
+++ 10_OOPS/mathPi.js
@@ -0,0 +1,2 @@
+Object.getOwnPropertyDescriptor() // Gets the own property descriptor of the specified object. 
+// An own property descriptor is one that is defined directly on the object and is not inherited from the object's prototype.
