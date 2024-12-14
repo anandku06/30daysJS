d6e239ab3db94192a1478973639493727075292e
Commiter <161126335+anandku06@users.noreply.github.com>
2024-12-15 01:14:43 +0530

    Add detailed comments and log property descriptor for Math.PI in mathPi.js

--- 10_OOPS/mathPi.js
+++ 10_OOPS/mathPi.js
@@ -3,3 +3,9 @@
 
 console.log(Math.PI)
 // Math.PI = 5  // value of Math.PI can't be overwritten
+// the writable property of PI is set to false that's why it can't be overwritten
+
+const descriptor = Object.getOwnPropertyDescriptor(Math, 'PI') // takes two parameters; one is the object and other is the property
+
+console.log(descriptor);
+// logs the whole description of the property
