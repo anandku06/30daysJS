d5c539d62b453c5b0ad0caa6a940ff70bae618c7
Commiter <161126335+anandku06@users.noreply.github.com>
2024-12-19 21:02:38 +0530

    Add initial content to test.js with JavaScript engine information and array types

---
+++ 11_funWithJS/test.js
@@ -0,0 +1,13 @@
+/* 
+JavaScript is mainly run with the help of V8 engine. V8 is a Google's open-source high-performance JavaScript and WebAssembly engine written in C++. 
+It is used in Chrome and in Node.js, among others.
+
+jsvu -> JavaScript Version Updater, makes it easy to install recent versions of various JavaScript engines without having to compile them from source.
+
+
+Array in JavaScript is of two types: Continuous and Holey 
+*/
+
+const myArr = []
+
+%DebugPrint(myArr)
